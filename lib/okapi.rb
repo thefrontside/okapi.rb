487a4b8d1682ed43dafd895688068dfa2d711870
Charles Lowell <cowboyd@frontside.io>
2017-09-25 16:07:57 -0500

    post to a login

--- lib/okapi.rb
+++ lib/okapi.rb
@@ -4,6 +4,7 @@ require 'open-uri'
 require 'json'
 
 module Okapi
+
   class Client
     def initialize(url, tenant_id = nil, authtoken = nil)
       @url = url
@@ -49,6 +50,28 @@ module Okapi
       end
     end
 
+    def post(path, body)
+      http = Net::HTTP.new(uri.host, uri.port)
+      if uri.scheme == "https"
+        http.use_ssl = true
+      end
+
+      http.start do
+        headers = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+        headers['X-Okapi-Tenant'] = @tenant_id if @tenant_id
+        headers['X-Okapi-Token'] = @authtoken if @authtoken
+        response = http.post(path, JSON.generate(body), headers)
+
+        RequestError.maybe_fail! response
+        json = JSON.parse(response.body)
+        if block_given?
+          yield json
+        else
+          json
+        end
+      end
+    end
+
     def with_tenant(tenant_id)
       client = Okapi::Client.new(@url, tenant_id)
       if block_given?
@@ -59,6 +82,76 @@ module Okapi
     end
 
     def with_authtoken(token)
+      client = self.class.new(@url, @tenant_id, token)
+      if block_given?
+        yield client
+      else
+        client
+      end
+    end
+
+    def login(options = {})
+      username = options[:username] || ''
+      password = options[:password] || ''
+      [username, password]
+    end
+
+
+    def login
+      @@login_module ||= define_okapi_module('login', 'authn', login: [ :create ], credentials: [ :index, :show, :create, :update ] )
+      @@login_module.new(self)
+    end
+
+    def configuration
+      @@configuration_module ||= define_okapi_module('configuration', 'configuration', entries: [ :index ])
+      @@configuration_module.new(self)
+    end
+
+    def define_okapi_module(interface_name, base_path, endpoints = {})
+
+      verb_methods = {
+        create: proc do |client, path, body|
+          client.post path, body
+        end,
+        index: proc do | client, path |
+          @client.get(path)
+        end,
+        update: proc do | client, path, id, body |
+          "update!"
+        end,
+        show: proc do | client, path, id |
+          @client.get("#{path}/#{id}")
+        end
+      }
+
+      Class.new.tap do |module_class|
+
+        module_class.class_eval do
+          def initialize(client)
+            @client = client
+          end
+        end
+
+        endpoints.each do |entry|
+          endpoint, verbs = entry
+
+          endpoint_class = Class.new
+          endpoint_class.class_eval do
+            def initialize(client)
+              @client = client
+            end
+          end
+          verbs.each do |verb|
+            method = verb_methods[verb]
+            fail "`#{verb}` is not a valid action" unless method
+            endpoint_class.send(:define_method, verb) do |*args|
+              method.call(@client, "/#{base_path}/#{endpoint}", *args)
+            end
+          end
+
+          module_class.send(:define_method, endpoint) { endpoint_class.new(@client) }
+        end
+      end
     end
   end
 
--- spec/okapi_spec.rb
+++ spec/okapi_spec.rb
@@ -18,18 +18,27 @@ RSpec.describe Okapi do
       expect(tenant.has_interface?("blip-bloop")).to be false
     end
 
-    it "can add a new configuration entry"
 
-    it "can update an existing configuration entry"
+    describe "a specific user" do
+      let(:token) { tenant.login.login.create(username: 'devolio', password: 'testpass') }
+      let(:user) { tenant.with_authtoken token }
 
-    it "can delete an existing configuration entry"
+      pending "list existing configuration entries" do
+        expect(user.configuration.entries.get)
+      end
 
-    describe "when the configuration interface doesn't exist" do
-      it "blows up in a helpful way"
-    end
+      it "can add a new configuration entry"
 
-    describe "when the tenant doesnt exist" do
-      it "blows up in a helpful way"
+      it "can update an existing configuration entry"
+
+      it "can delete an existing configuration entry"
+      describe "when the configuration interface doesn't exist" do
+        it "blows up in a helpful way"
+      end
+
+      describe "when the tenant doesnt exist" do
+        it "blows up in a helpful way"
+      end
     end
   end
 end
