007f21dde29bdf8ea2cdc0e0122e653a0489a215
Charles Lowell <cowboyd@frontside.io>
2017-10-06 12:07:25 -0500

    Empty check environment variables
    *speculatively*, on Linux non-present environment variables are just
     empty strings and not `nil`. This abstracts instance and environment
     access for every setting so that it can check to make sure that the
     environment variable is actually present.

--- lib/okapi/settings.rb
+++ lib/okapi/settings.rb
@@ -6,18 +6,18 @@ module Okapi
       @token = token
     end
     def url
-      ENV['OKAPI_URL'] || @url or raise ConfigurationError, <<-EOM
+      get_var!(:url, <<~EOM) do |url|
 this operation requires the url of your Okapi gateway, but it couldn't be found.
 
 You can fix this by setting either the `OKAPI_URL` environment variable, or
 using the `--url` option if you're using the command line.
---
 EOM
-      URI(ENV['OKAPI_URL'] || @url)
+        URI(url)
+      end
     end
 
     def tenant
-      ENV['OKAPI_TENANT'] || @tenant or raise ConfigurationError, <<-EOM
+      get_var!(:tenant, <<~EOM)
 this operation requires a tenant id, but it couldn't be found.
 
 You can fix this by setting either the `OKAPI_TENANT` environment variable, or
@@ -26,7 +26,7 @@ EOM
     end
 
     def token
-      ENV['OKAPI_TOKEN'] || @token or raise ConfigurationError, <<-EOM
+      get_var!(:token, <<~EOM)
 this operation requires you to be logged in, and already authenticated with
 your Okapi cluster.
 
@@ -34,6 +34,19 @@ You can fix this by obtaining an authenication token, and then using it by
 either setting the `OKAPI_TOKEN` environment variable or using the
 `--token` option from the command line.
 EOM
+    end
+
+    def get_var!(symbol, error_msg)
+      env_var_name = "OKAPI_#{symbol.to_s.upcase}"
+      env_value = ENV[env_var_name]
+      instance_value = instance_variable_get("@#{symbol}")
+      if !env_value.nil? && !env_value.trim.empty?
+        block_given? ? yield(env_value) : env_value
+      elsif instance_value
+        block_given? ? yield(instance_value) : instance_value
+      else
+        raise ConfigurationError, error_msg
+      end
     end
   end
 
