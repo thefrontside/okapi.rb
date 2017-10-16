198165658a6232f58093b457370f22a0f46a0e2e
Charles Lowell <cowboyd@frontside.io>
2017-10-16 12:57:52 -0500

    Delete and Post from the CLI
    In the beginning, there was only `get`, `tenant:get`, and `user:get`,
    which was great for readonly operations, but we need, in our
    interaction with mod-configuration to delete all the configuration
    entries and then create them again.
    
    This adds two new commands to the CLI (and the ruby client): `create`
    to issue POST requests. You can pass the body of the newly minted
    resource via stdin:
    
    ```
    $ echo '{"some": "json"}' | okapi create /path/to/some/resource
    ```
    
    By the same token, you can issue DELETE requests from the client, and
    from the command line as well:
    
    ```
    $ okapi destroy /path/to/some/resource
    ```

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,8 +34,9 @@ RSpec.configure do |config|
   end
 end
 
+
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  config.filter_sensitive_data("<OKAPI_TEST_TOKEN>") { ENV["OKAPI_TEST_TOKEN"] }
+  config.default_cassette_options = { record: :new_episodes }
 end
